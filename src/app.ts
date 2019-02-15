0398429a1338ba0366955ed006e73f78363b5e26
moriyuu <moriyu54321@gmail.com>
2019-02-16 07:14:11 +0900

    add: base path `/v1`

--- src/app.ts
+++ src/app.ts
@@ -8,13 +8,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/v1/", (req, res) => {
   res.send("Hello World");
 });
-app.post("/balances", balanceController.createBalance);
-app.get("/balances/:balanceId", balanceController.showBalance);
-app.post("/deposits", depositController.createDeposit);
-app.get("/deposits", depositController.listDeposits);
-app.get("/deposits/:depositId", depositController.showDeposit);
+app.post("/v1/balances", balanceController.createBalance);
+app.get("/v1/balances/:balanceId", balanceController.showBalance);
+app.post("/v1/deposits", depositController.createDeposit);
+app.get("/v1/deposits", depositController.listDeposits);
+app.get("/v1/deposits/:depositId", depositController.showDeposit);
 
 app.listen(3000);
