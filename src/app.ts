18d89a049edda4ce8316b8215ea6f7aa51934a1b
moriyuu <moriyu54321@gmail.com>
2019-02-16 07:37:10 +0900

    update: separate app and server

--- src/app.ts
+++ src/app.ts
@@ -8,13 +8,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/v1/", (req, res) => {
-  res.send("Hello World");
-});
 app.post("/v1/balances", balanceController.createBalance);
 app.get("/v1/balances/:balanceId", balanceController.showBalance);
 app.post("/v1/deposits", depositController.createDeposit);
 app.get("/v1/deposits", depositController.listDeposits);
 app.get("/v1/deposits/:depositId", depositController.showDeposit);
 
-app.listen(3000);
+export default app;
