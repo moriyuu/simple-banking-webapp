eb5e005757cdbd03a3d71493f931bbf3f9a6bf77
moriyuu <moriyu54321@gmail.com>
2019-02-16 09:25:44 +0900

    add: balances test

---
+++ src/test/balance.test.ts
@@ -0,0 +1,42 @@
+import request from "supertest";
+import app from "../app";
+
+describe("POST /v1/balances", () => {
+  it("should response 200 OK", async done => {
+    const res = await request(app)
+      .post("/v1/balances")
+      .send({
+        balanceId: "foobar",
+        amount: 2000
+      });
+    expect(res.status).toBe(200);
+    expect(res.body.balance.id).toBe("foobar");
+    expect(res.body.balance.id).toBe("foobar");
+    expect(res.body.balance.amount).toBeGreaterThanOrEqual(0);
+    expect(new Date(res.body.balance.createdAt).getTime()).toBeLessThan(
+      new Date().getTime()
+    );
+    done();
+  });
+});
+
+describe("GET /v1/balances/:balanceId", () => {
+  it("should response 200 OK", async done => {
+    const balanceId = "valid-balanceId";
+    const res = await request(app).get(`/v1/balances/${balanceId}`);
+    expect(res.status).toBe(200);
+    expect(res.body.balance.id).toBe("valid-balanceId");
+    expect(res.body.balance.amount).toBeGreaterThanOrEqual(0);
+    expect(new Date(res.body.balance.createdAt).getTime()).toBeLessThan(
+      new Date().getTime()
+    );
+    done();
+  });
+
+  it("should response 404 Not Found", async done => {
+    const balanceId = "nonexistent-balanceId";
+    const res = await request(app).get(`/v1/balances/${balanceId}`);
+    expect(res.status).toBe(404);
+    done();
+  });
+});
